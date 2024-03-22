71e4af04de45797c1ff31b0f4adfe491c67c3e39
Frankosviz <geom.francescopaolo@gmail.com>
2024-03-22 18:26:52 +0100

    snack 2

---
+++ Snack - 2/js/script.js
@@ -0,0 +1,61 @@
+//  SNACK 2
+// Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+// 1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+// ES (Marco della Rovere => MARCO DELLA ROVERE);
+// 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+// 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+// Buon pomeriggio e buon lavoro !!! :faccia_nerd:
+// Questo è l'elenco degli studenti:
+// Id  Name                Grades
+// 213 Marco della Rovere      78
+// 110 Paola Cortellessa       96
+// 250 Andrea Mantegna         48
+// 145 Gaia Borromini          74
+// 196 Luigi Grimaldello       68
+// 102 Piero della Francesca   50
+// 120 Francesca da Polenta    84
+
+const listaStudenti = [
+    {
+        id : 213,
+        nome : 'Marco della Rovere',
+        voto : 78
+    },
+    {
+        id : 110,
+        nome : 'Paola Cortellessa',
+        voto : 96
+    },
+    {
+        id : 250,
+        nome : 'Andrea Mantegna',
+        voto : 48
+    },
+    {
+        id : 145,
+        nome : 'Gaia Borromini',
+        voto : 74
+    },
+    {
+        id : 196,
+        nome : 'Luigi Grimaldello',
+        voto : 68
+    },
+    {
+        id : 102,
+        nome : 'Piero della Francesca',
+        voto : 50
+    },
+    {
+        id : 120,
+        nome : 'Francesca da Polenta ',
+        voto : 84
+    }
+]
+
+const listaNomiMaiuscolo = listaStudenti.map(studente => studente.nome.toUpperCase());
+
+console.log(listaNomiMaiuscolo);
+
+const listaStudentiMag70 = 
+const listaStudentiMin70 = 
